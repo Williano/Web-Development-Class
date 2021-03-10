066a52f62ccce70382ceef5d7c5d5a8cfa9ba980
Williano <paawilly17@gmail.com>
2021-03-09 22:10:30 -0600

    Add cart funtionality using sessionstorage

--- assignment-3/js/add_to_cart.js
+++ assignment-3/js/add_to_cart.js
@@ -1,12 +1,30 @@
 function addToCart() {
+  var cart = {};
+
   var productName = document.getElementsByClassName("product-name")[0]
     .innerHTML;
+  var productImage = document.getElementsByTagName("img")[0].src;
+  var productPrice = document.getElementById("product-price").innerHTML;
+  var productQuantity = document.getElementById("quantity").value;
 
-  var userInput = confirm(`Add ${productName} to cart?`);
-
-  if (userInput == true) {
-    location.href = "../../store_index.html";
+  if (productQuantity == "") {
+    alert("Product quantity should not be empty");
   } else {
-    window.location("#");
+    cart["productName"] = productName;
+    cart["productImage"] = productImage;
+    cart["productPrice"] = productPrice;
+    cart["productQuantity"] = productQuantity;
+
+    sessionStorage.setItem("cart", JSON.stringify(cart));
+
+    alert("Item added to cart successfully.");
+
+    var userInput = confirm(`Add ${productName} to cart?`);
+
+    if (userInput == true) {
+      location.href = "../../store_index.html";
+    } else {
+      location.href = "#";
+    }
   }
 }
